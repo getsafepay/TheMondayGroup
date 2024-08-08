d948f3b1bc414d9171de8f26bbdcf45ce2366021
bilalqureshi <bilalqureshi92@gmail.com>
2024-08-08 22:56:45 +0500

    build issues

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"OBBG/8qgxsOh5kOPUCvDIU2ShTVWvWrcDBRBsJTWqzI=\"}"
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Container } from "@/components/Foundations";
 import { Transition } from "@/components/Transition";
 
